d72a6c3edebd5b0c3110b052d5c8babb313a9625
Alejandro Machado <alemacgo@gmail.com>
2016-01-20 16:27:38 +0100

    Establish ArticleListVC as the data delegate and data source of news

---
+++ CaracasChronicles-iOS/ArticleListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CaracasChronicles-iOS
+//
+//  Created by Alejandro on 1/20/16.
+//
+//
+
+import UIKit
+
+class ArticleListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ArticleListViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "ArticleCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        return cell
+    }
+}
