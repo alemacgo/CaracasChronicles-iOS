831459298c06538985792bc1a83e68a0c4d37a26
Alejandro Machado <alemacgo@gmail.com>
2016-01-21 15:13:43 +0100

    Prevent loading indicator from showing on Article List
    The indicator would show if the user navigated back from an Article VC with a
    webview that hadn't finished loading.

--- CaracasChronicles-iOS/ArticleListViewController.swift
+++ CaracasChronicles-iOS/ArticleListViewController.swift
@@ -27,6 +27,10 @@ class ArticleListViewController: UIViewController {
         request()
     }
     
+    override func viewWillAppear(animated: Bool) {
+        SVProgressHUD.dismiss()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
